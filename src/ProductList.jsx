f15d4d6b95c22e0f71c92a2592c2fa5876fcea5f
Aditya Yadav <adityayadav2903@gmail.com>
2025-06-27 16:39:03 -0400

    new commit

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { addItem } from "../CartSlice";
+import { addItem } from "./CartSlice";
 import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
 import './ProductList.css'
